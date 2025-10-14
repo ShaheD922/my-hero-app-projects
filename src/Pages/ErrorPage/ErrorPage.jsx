4abed012c630b7956b509508fb3b72442c477fd2
ShaheD922 <shahedhasan.yc@gmail.com>
2025-10-15 01:20:21 +0600

    added an footer and navbar

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import { Link, Navigate } from "react-router";
+import notFount from "../../assets/error-404.png";
 
 const ErrorPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <img src={notFount} alt="" />
+      </div> 
+      <div>
+        <p className="text-5xl font-bold py-5">Sorry,this page is not found!</p>
+        <p className="text-xl text-gray-600 pb-5">
+          The page you are looking for is not available.
+        </p>
+        <Link
+          onClick={() => Navigate(-1)}
+          className="bg-violet-700 hover:bg-violet-600 text-white font-bold py-2.5 px-4 rounded shadow-lg transition duration-200  justify-between"
+        >
+          Go Back!
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default ErrorPage;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router";
 import Home from "../Pages/Home/Home";
 import Root from "../Pages/Root/Root";
 import AllApps from "../Pages/AllApps/AllApps";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -12,7 +13,17 @@ const router = createBrowserRouter([
         index: true,
         element: <Home></Home>,
       },
-      
+      {
+        path:"allApps",
+       element: <AllApps></AllApps>
+      },
+
+
+
+      {
+        path: "*",
+        element: <ErrorPage />,
+      },
     ],
   },
 ]);
